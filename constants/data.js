ae8a6a5f63abfb697351f07374097ce631dd3237
Adebayo Adeoye <adeoyeadebayo18@gmail.com>
2023-05-18 14:16:02 +0100

    first commit

---
+++ constants/data.js
@@ -0,0 +1,78 @@
+const userData = {
+  githubUsername: "Pfactorial01",
+  name: "Adebayo Adeoye",
+  designation: "Backend Developer",
+  avatarUrl: "/avatar.jpg",
+  email: "adeoyeadebayo18@gmail.com",
+  phone: "+2348145878086",
+  address: "Lagos, Nigeria",
+  projects: [
+    {
+      title: "Envue",
+      link: "https://envue.vercel.app/",
+      imgUrl: "/envue.png",
+    },
+    {
+      title: "Geochat",
+      link: "https://geochat1.netlify.app/",
+      imgUrl: "/geochat.png",
+    },
+    {
+      title: "Project_flow",
+      link: "",
+      imgUrl: "/project_flow.png",
+    },
+    {
+      title: "clone-tube",
+      link: "https://clone-tube.netlify.app/",
+      imgUrl: "/clone_tube.png",
+    },
+    {
+      title: "jumai-store",
+      link: "https://jumai-store.netlify.app/",
+      imgUrl: "/jumai.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that provide value",
+    description: [
+      `I've been building software for the past 2 years`,
+      `I am proficient in Javascript, Node.js, Express.js, Python, Flask, Django, relational and non-relational databases and AWS`,
+      `I am problem solver at core with attention to detail and my long term goal is to improve myself and the value of any company or project I find myself in.`,
+    ],
+    currentProject: "Envue",
+    currentProjectUrl: "https://envue.vercel.app/",
+  },
+  experience: [
+    {
+      title: "Software Developer",
+      company: "Retink",
+      year: "2023",
+      companyLink: "https://retink.io/",
+      desc: "Contributed to our flagship content creation platform.",
+    },
+    {
+      title: "Software Developer",
+      company: "Trusted Partners",
+      year: "2022",
+      companyLink: "https://trusted.sa/",
+      desc: "Contributed to the development of Brand Protection, a cyber security web application",
+    },
+    {
+      title: "Freelancer",
+      company: "Upwork",
+      year: "2021",
+      companyLink: "https://www.upwork.com/freelancers/~017465ccf80bd0e2ce",
+      desc: "Worked as a freelancer on upwork with numerous succesful projects",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1PCI2dLDcr12hSeVucjtGTZlKpX_0aaKk/view?usp=sharing",
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/adeoyeadebayo/",
+    github: "https://github.com/Pfactorial01",
+  },
+};
+
+export default userData;
